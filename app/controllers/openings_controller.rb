9a28d9e0a467914464f8dc287d53c9e041bb6826
Luis Correa d'Almeida <luis.ca@gmail.com>
2009-11-23 20:45:39 +0000

    Added ability to edit opening

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -43,12 +43,58 @@ class OpeningsController < ApplicationController
         @contract.save!
       end
     
-      flash[:notice] = 'Opening was successfully created.'
-      
-      redirect_to(organization_path(@organization))
+      flash[:success] = 'Opening was successfully created.'
+      redirect_to(organization_opening_path(@organization, @contract))
     rescue
-      raise
       render :template => "/openings/new.haml"
     end
   end  
+
+  def edit
+    @opening = Contract.find(params[:id])
+    @organization = @opening.position.organization
+  end
+  
+  def update
+    begin
+      ActiveRecord::Base.transaction do
+        @organization = Organization.find(params[:organization_id])
+        @position = _find_or_create_position(@organization, params[:position])
+        
+        @contract = Contract.find(params[:id])
+        @contract.position_id = @position.id
+        @contract.description = params[:contract][:description]
+        
+        if params[:date][:asap].blank?
+          @contract.from_month = params[:date][:from_month]
+          @contract.from_year = params[:date][:from_year]
+        end
+        
+        if params[:date][:open].blank?
+          @contract.to_month = params[:date][:to_month]
+          @contract.to_year = params[:date][:to_year]
+        end
+        
+        @contract.posted_by_user_id = current_user.id
+        @contract.save!
+      end
+    
+      flash[:success] = 'Opening was successfully updated.'
+      redirect_to(organization_opening_path(@organization, @contract))
+    rescue
+      raise
+      render :template => "/openings/edit.haml"
+    end
+  end
+  
+  def _find_or_create_position(organization, params)
+    if params[:id].blank?
+      position = Position.new(params)
+      position.save!
+      organization.positions << position
+    else
+      position = Position.find(params[:id])
+    end
+    position
+  end
 end
