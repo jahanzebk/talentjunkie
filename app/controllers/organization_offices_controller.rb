7d2753e3ffdaae0ee0f6f61079f6a2aa4d89d3e4
Luis Correa d'Almeida <luis.ca@gmail.com>
2010-01-05 23:51:48 +0000

    Added edit office

--- app/controllers/organization_offices_controller.rb
+++ app/controllers/organization_offices_controller.rb
@@ -18,16 +18,26 @@ class OrganizationOfficesController < ApplicationController
     end
   end
   
+  def edit
+    @organization_office = OrganizationOffice.find(params[:id])
+    @html_content = render_to_string :partial => "/organization_offices/edit.haml"
+    respond_to do |format|
+      format.js
+    end
+  end
+  
   def create
     begin
       @organization = Organization.find(params[:organization_id])
       
-      @city = City.find(params[:address][:city][:id])
-      @address = Address.factory(params[:address], @city)
-      @address.save!
+      ActiveRecord::Base.transaction do
+        @city = City.find(params[:address][:city][:id])
+        @address = Address.factory(params[:address], @city)
+        @address.save!
       
-      @organization_office = OrganizationOffice.new(:organization_id => @organization.id, :name => params[:organization_office][:name], :address_id => @address.id)
-      @organization_office.save!
+        @organization_office = OrganizationOffice.new(:organization_id => @organization.id, :name => params[:organization_office][:name], :address_id => @address.id)
+        @organization_office.save!
+      end
       
       render :json => {:url => organization_offices_path(@organization)}.to_json, :status => 201
     rescue ActiveRecord::RecordNotFound => e
@@ -39,4 +49,32 @@ class OrganizationOfficesController < ApplicationController
     end
   end
   
+  
+  def update
+    begin
+      ActiveRecord::Base.transaction do
+        @organization_office = OrganizationOffice.find(params[:id])
+        @organization_office.name = params[:organization_office][:name]
+        @organization_office.save!
+        
+        @city = City.find(params[:address][:city][:id])
+        
+        @address = @organization_office.address
+        @address.address_1 = params[:address][:address_1]
+        @address.address_2 = params[:address][:address_2]
+        @address.postal_code = params[:address][:postal_code]
+        @address.city_id = @city.id
+        @address.save!
+      end      
+      
+      render :json => {:url => organization_offices_path(@organization_office.organization)}.to_json, :status => 201
+    rescue ActiveRecord::RecordNotFound => e
+      errors = {:address_city => [['name', "can't be blank"]]}
+      render :json => errors.to_json, :status => 406
+    rescue
+      # raise collect_errors_for(@address, @organization_office).to_json.inspect
+      render :json => collect_errors_for(@address, @organization_office).to_json, :status => 406
+    end
+  end
+
 end
--- app/models/organization_office.rb
+++ app/models/organization_office.rb
@@ -1,4 +1,5 @@
 class OrganizationOffice < ActiveRecord::Base
+  belongs_to :organization
   belongs_to :address
   
   validates_presence_of :name, :organization_id, :address_id
