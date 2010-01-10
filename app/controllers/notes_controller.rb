3d938c08120d822d88420cb17a3e561b1d725bfc
Luis Correa d'Almeida <luis.ca@gmail.com>
2010-01-10 12:49:34 +0000

    Fixed bug in notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,7 +7,8 @@ class NotesController < ApplicationController
   
   def create
     @user = User.find(params[:user_id])
-    @note = Note.new(:user_id => current_user.id, :object_id => @user.id, :content => params[:notes][:content])
+    @note = Note.new(:user_id => current_user.id, :object_id => @user.id)
+    @note.content = params[:notes][:content]
     @note.save!
     
     redirect_to "#{person_path(@user)}/notes"
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ActiveRecord::Base
   belongs_to :user
   
   def content=(text)
-    self[:summary] = RedCloth.new(text || "")
-    self[:summary].sanitize_html = true
+    self[:content] = RedCloth.new(text || "")
+    self[:content].sanitize_html = true
   end
 end
