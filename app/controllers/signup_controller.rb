234bffcef66d2877f776dc3aa2e0aa031cd22881
Luis Correa d'Almeida <luis.ca@gmail.com>
2010-01-17 16:50:01 +0000

    Working on welcome page

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -3,22 +3,25 @@ class SignupController < ApplicationController
   skip_before_filter :check_authentication
   
   def create
-    @user = SimpleUser.new
-    @user.first_name = params[:simple_user][:first_name]
-    @user.last_name = params[:simple_user][:last_name]
-    @user.primary_email = params[:simple_user][:primary_email]
-    @user.password = params[:simple_user][:password]
+    ActiveRecord::Base.transaction do
+      @user = SimpleUser.new
+      @user.first_name = params[:simple_user][:first_name]
+      @user.last_name = params[:simple_user][:last_name]
+      @user.primary_email = params[:simple_user][:primary_email]
+      @user.password = params[:simple_user][:password]
 
-    @user.detail = UserDetail.create!
+      @user.detail = UserDetail.create!
+      @user.settings = UserSetting.create!
 
-    begin
-      @user.save!
-      @user.steps << AchievementStep.find(1)
+      begin
+        @user.save!
+        @user.steps << AchievementStep.find(1)
       
-      session[:user] = @user.id
-      render :json => {:url => person_path(@user)}.to_json, :status => 201
-    rescue
-      render :json => collect_errors_for(@user).to_json, :status => 406
+        session[:user] = @user.id
+        render :json => {:url => person_path(@user)}.to_json, :status => 201
+      rescue
+        render :json => collect_errors_for(@user).to_json, :status => 406
+      end
     end
   end
   
