50cf9661488442b4f37d1711ccbe4e1892ec2c2d
Luis Correa d'Almeida <luis.ca@gmail.com>
2010-02-19 18:32:15 +0000

    Fixed bug

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,7 @@ module EventsHelper
   def render_event_title(event)
     case event.type.to_s
       when "Events::StartFollowingPeople"
-        object_profile_link = link_to event.object.full_name, "/profile/#{event.object_id}"
+        object_profile_link = link_to event.object.full_name, "/people/#{event.object_id}"
         "#{event.subject.first_name} is now following #{object_profile_link}"
       when "Events::StartFollowingOrganizations"
         object_profile_link = link_to event.object.name, "/organizations/#{event.object_id}"
